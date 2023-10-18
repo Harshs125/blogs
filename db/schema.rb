6152726d011cec6bc8ffb541c21160f53bd4e1eb
harshsoni <harsh.soni@joshsoftware.com>
2023-10-18 16:09:54 +0530

    0.8 rails model: send the welcome  to the user after  a successfull registeration

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_18_065821) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_18_093414) do
   create_table "Articles", force: :cascade do |t|
     t.string "article_title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "created_by"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
