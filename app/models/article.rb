7a3dab26ffb93ec6cc8d35e75267b4e8156f0d25
harshsoni <harsh.soni@joshsoftware.com>
2023-10-19 12:32:39 +0530

    add comment and delete comment on article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+    has_many :comments
+
     validates :article_title, presence: true
     validates :body, presence: true, length: { minimum: 10 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_18_093414) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_19_061300) do
   create_table "Articles", force: :cascade do |t|
     t.string "article_title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "created_by"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "article_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -27,4 +36,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_18_093414) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "articles"
 end
